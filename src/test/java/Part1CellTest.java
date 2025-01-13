527852898d41daeedd97c148a9167f1295ced230
yair vaisberg <yairvaisberg@gmail.com>
2025-01-13 20:48:53 +0200

    init tests

--- src/test/java/Part1CellTest.java
+++ src/test/java/Part1CellTest.java
@@ -76,14 +76,33 @@ class Part1CellTest {
             Part1Cell part1Cell8 = new Part1Cell("123+A5");
             Part1Cell part1Cell9 = new Part1Cell("48484");
 
+            assertTrue(part1Cell.isForm("=1+1"));
+            assertTrue(part1Cell1.isForm("=A4+5"));
+            assertTrue(part1Cell2.isForm("=9*9"));
+            assertTrue(part1Cell3.isForm("=4"));
 
 
-
-
+            assertFalse(part1Cell4.isForm("Text with spaces"));
+            assertFalse(part1Cell5.isForm("123"));
+            assertFalse(part1Cell6.isForm("sdsad"));
+            assertFalse(part1Cell8.isForm("123+A5"));
+            assertFalse(part1Cell9.isForm("48484"));
 
     }
 
     @org.junit.jupiter.api.Test
     void computeForm() {
+
+        Part1Cell part1Cell = new Part1Cell("=1+1");
+        Part1Cell part1Cell1 = new Part1Cell("=(6+9)+5");
+        Part1Cell part1Cell2 = new Part1Cell("=(9+1)*9");
+        Part1Cell part1Cell3 = new Part1Cell("=4");
+        Part1Cell part1Cell4 = new Part1Cell("=4+(4+(4+(4+(4))))");
+
+        assertEquals(2.0,part1Cell.computeForm("=1+1"));
+        assertEquals(20.0,part1Cell1.computeForm("=(6+9)+5"));
+        assertEquals(90.0,part1Cell2.computeForm("=(9+1)*9"));
+        assertEquals(4.0,part1Cell3.computeForm("=4"));
+        assertEquals(20.0,part1Cell4.computeForm("=4+(4+(4+(4+(4))))"));
     }
 }
